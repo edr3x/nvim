edd29021e9b5261d77100c467677694f8dc64bf0
edr3x <dhunganaannanta@gmail.com>
2025-03-23 01:49:30 +0545

    refactor: back to basics
    refactor: merge files

--- lua/r3x/plugins/init.lua
+++ lua/r3x/plugins/init.lua
@@ -1,5 +1,15 @@
 return {
     {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {},
+    },
+    {
         "towolf/vim-helm",
         ft = "yaml",
     },

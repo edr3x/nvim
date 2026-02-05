0c28afd78d86d27803d6b94c7a097b5b41348fe8
edr3x <dhunganaannanta@gmail.com>
2026-02-05 17:50:48 +0545

    feat: use treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,81 +1,76 @@
+local ts_init = function()
+    local ensure_installed = {
+        "bash",
+        "c",
+        "cpp",
+        "css",
+        "dart",
+        "dockerfile",
+        "fish",
+        "go",
+        "graphql",
+        "hcl",
+        "helm",
+        "html",
+        "java",
+        "javascript",
+        "json",
+        "llvm",
+        "lua",
+        "make",
+        "markdown",
+        "prisma",
+        "proto",
+        "python",
+        "query",
+        "rust",
+        "scss",
+        "svelte",
+        "sql",
+        "terraform",
+        "toml",
+        "tsx",
+        "typescript",
+        "vimdoc",
+        "vue",
+        "yaml",
+        "zig",
+    }
+
+    local isnt_installed = function(lang)
+        return #vim.api.nvim_get_runtime_file("parser/" .. lang .. ".*", false) == 0
+    end
+
+    local to_install = vim.tbl_filter(isnt_installed, ensure_installed)
+    if #to_install > 0 then
+        require("nvim-treesitter").install(to_install)
+    end
+
+    -- Ensure tree-sitter enabled after opening a file for target language
+    local filetypes = {}
+    for _, lang in ipairs(ensure_installed) do
+        for _, ft in ipairs(vim.treesitter.language.get_filetypes(lang)) do
+            table.insert(filetypes, ft)
+        end
+    end
+
+    vim.api.nvim_create_autocmd("FileType", {
+        desc = "Start treesitter",
+        group = vim.api.nvim_create_augroup("start_treesitter", { clear = true }),
+        pattern = filetypes,
+        callback = function(ev)
+            vim.treesitter.start(ev.buf)
+        end,
+    })
+end
+
 return {
     "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    branch = "main",
     build = ":TSUpdate",
     dependencies = {
         { "nvim-treesitter/nvim-treesitter-context", config = true },
     },
-    ---@diagnostic disable
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "bash",
-                "c",
-                "cpp",
-                "css",
-                "dart",
-                "dockerfile",
-                "fish",
-                "go",
-                "graphql",
-                "hcl",
-                "helm",
-                "html",
-                "java",
-                "javascript",
-                "json",
-                "llvm",
-                "lua",
-                "make",
-                "markdown",
-                "prisma",
-                "proto",
-                "python",
-                "query",
-                "rust",
-                "scss",
-                "svelte",
-                "sql",
-                "terraform",
-                "toml",
-                "tsx",
-                "typescript",
-                "vimdoc",
-                "vue",
-                "yaml",
-                "zig",
-            },
-            ignore_install = { "php" },
-            sync_install = false,
-            auto_install = false,
-            autotag = {
-                enable = true,
-            },
-            highlight = {
-                enable = true,
-                extended_mode = true,
-                disable = function(_, buf)
-                    local max_filesize = 200 * 1024 -- 200 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-                additional_vim_regex_highlighting = false,
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<c-space>",
-                    node_incremental = "<c-space>",
-                    scope_incremental = "<c-s>",
-                    node_decremental = "<c-backspace>",
-                },
-            },
-            query_linter = {
-                enable = true,
-                use_virtual_text = true,
-                lint_events = { "BufWrite", "CursorHold" },
-            },
-        })
-    end,
+    init = ts_init(),
 }
